8555716688c474194ed3b41835a6a07657ac5a58
Jhon Torres <johntorres2408@gmail.com>
2026-09-12 22:44:52 -0500

    inicializacion de proyecto via springboot

---
+++ CentroDeportivo/src/main/java/com/example/CentroDeportivo/CentroDeportivoApplication.java
@@ -0,0 +1,13 @@
+package com.example.CentroDeportivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentroDeportivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentroDeportivoApplication.class, args);
+	}
+
+}
---
+++ CentroDeportivo/src/test/java/com/example/CentroDeportivo/CentroDeportivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CentroDeportivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentroDeportivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
