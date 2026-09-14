7b800596cfc0e85a437cd76f3bfaeb32ff0fe9cf
Camilo_WH <yefersoncamiloblanco@gmail.com>
2026-09-14 15:52:40 -0500

    Se agregó consultas personalizadas en ListaEsperaRepository + creación de ListaEsperaServiceImp

--- CentroDeportivo/src/main/java/com/example/CentroDeportivo/Repository/ListaEsperaRepository.java
+++ CentroDeportivo/src/main/java/com/example/CentroDeportivo/Repository/ListaEsperaRepository.java
@@ -4,8 +4,18 @@ import com.example.CentroDeportivo.Entity.ListaEspera;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ListaEsperaRepository extends JpaRepository<ListaEspera, Long> {
     List<ListaEspera> findByActividadIdOrderByPosicionAsc(Long actividadId);
+
+    //Obtiene la posición más alta el número de turno mayor ocupada actualmente
+
+    Optional<Integer> findMaxPosicionByActividadId(Long actividadId);
+
+
+     // Busca la primera persona en la lista de espera de una actividad que se encuentre.
+
+    Optional<ListaEspera> findFirstByActividadIdAndEstadoOrderByPosicionAsc(Long actividadId, String enEspera);
 }
